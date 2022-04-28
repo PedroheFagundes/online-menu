52e309e980ec2058b3fbfb2a5d28da37992093e7
Pedro Fagundes <pedrohefagundes@gmail.com>
2022-04-28 11:54:20 +0100

    teste

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>Online Menu teste heroku deploy 2</h1>
+    <h1>Online Menu teste heroku deploy 3</h1>
   )
 }
 
--- server/index.ts
+++ server/index.ts
@@ -13,5 +13,5 @@ const PORT = process.env.PORT || 5000;
 
 // Create a server running on chosen PORT
 app.listen(PORT, () => {
-  console.log(`test2 server has start on PORT ${PORT}`);
+  console.log(`test3 server has start on PORT ${PORT}`);
 });
