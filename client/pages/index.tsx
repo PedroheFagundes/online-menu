3ac770706b021ed29315bd6e714542c585a72806
Pedro Fagundes <pedrohefagundes@gmail.com>
2022-05-06 11:30:52 +0100

    navbar z-indez fixing

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,6 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className='container-fluid p-0 position-fixed d-flex justify-content-center || bg w414 h100pc'>
       <div className="col-11 d-flex flex-column || w414 h100pc">
+      <div className="h55" />
         <div className='justify-content-center fw-bold d-flex align-items-center || h100 font-satisfy fs40'>O que deseja pedir?</div>
         <div className="flex-column d-flex justify-content-around || htotal">
           <SectionCard section='starters' position='start' name='Entradas'/>
