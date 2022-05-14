9e1b5573cff7a7dfedc1162a006352ccdede6e56
Pedro Fagundes <pedrohefagundes@gmail.com>
2022-05-14 20:35:13 +0100

    query update to change languages

--- server/routes/products.js
+++ server/routes/products.js
@@ -68,7 +68,7 @@ router.get('/starters-en', async (req, res) => {
 	}
 });
 
-router.get('/main-dishes', async (req, res) => {
+router.get('/main-dishes-pt', async (req, res) => {
 	try {
 		const mainDishes = await pool.query(`
 			select
@@ -87,6 +87,131 @@ router.get('/main-dishes', async (req, res) => {
 			`
 		);
 		res.json(mainDishes.rows);
+	} 
+	catch (err) {
+		console.error(err.message);
+	}
+});
+
+router.get('/main-dishes-en', async (req, res) => {
+	try {
+		const mainDishes = await pool.query(`
+			select
+				sub.name as sub_section,
+				prod.name,
+				description,
+				replace (price::text, '.', ',') as price
+			from
+				product_en prod
+				join sub_section_en sub on
+					prod.sub_section_key = sub.key
+			where
+				sub.section_id = 2
+			order by
+				sub_section
+			`
+		);
+		res.json(mainDishes.rows);
+	} 
+	catch (err) {
+		console.error(err.message);
+	}
+});
+
+router.get('/desserts-pt', async (req, res) => {
+	try {
+		const desserts = await pool.query(`
+			select
+				sub.name as sub_section,
+				prod.name,
+				description,
+				replace (price::text, '.', ',') as price
+			from
+				product_pt prod
+				join sub_section_pt sub on
+					prod.sub_section_key = sub.key
+			where
+				sub.section_id = 3
+			order by
+				sub_section
+			`
+		);
+		res.json(desserts.rows);
+	} 
+	catch (err) {
+		console.error(err.message);
+	}
+});
+
+router.get('/desserts-en', async (req, res) => {
+	try {
+		const desserts = await pool.query(`
+			select
+				sub.name as sub_section,
+				prod.name,
+				description,
+				replace (price::text, '.', ',') as price
+			from
+				product_en prod
+				join sub_section_en sub on
+					prod.sub_section_key = sub.key
+			where
+				sub.section_id = 3
+			order by
+				sub_section
+			`
+		);
+		res.json(desserts.rows);
+	} 
+	catch (err) {
+		console.error(err.message);
+	}
+});
+
+router.get('/drinks-pt', async (req, res) => {
+	try {
+		const drinks = await pool.query(`
+			select
+				sub.name as sub_section,
+				prod.name,
+				description,
+				replace (price::text, '.', ',') as price
+			from
+				product_pt prod
+				join sub_section_pt sub on
+					prod.sub_section_key = sub.key
+			where
+				sub.section_id = 4
+			order by
+				sub_section
+			`
+		);
+		res.json(drinks.rows);
+	} 
+	catch (err) {
+		console.error(err.message);
+	}
+});
+
+router.get('/drinks-en', async (req, res) => {
+	try {
+		const drinks = await pool.query(`
+			select
+				sub.name as sub_section,
+				prod.name,
+				description,
+				replace (price::text, '.', ',') as price
+			from
+				product_en prod
+				join sub_section_en sub on
+					prod.sub_section_key = sub.key
+			where
+				sub.section_id = 4
+			order by
+				sub_section
+			`
+		);
+		res.json(drinks.rows);
 	} 
 	catch (err) {
 		console.error(err.message);
